f2cd9e0cc899336c898d75c7949505b05c03966b
Dullestdear <prakharbhatnagar619@gmail.com>
2026-03-08 12:21:58 +0530

    Building Assets
    Created and applied assets to create the exterior of the building

--- Assets/Scripts/Elevator.cs
+++ Assets/Scripts/Elevator.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro; // For the text display later
+using TMPro;
 
 public class Elevator : MonoBehaviour
 {
@@ -10,7 +10,8 @@ public class Elevator : MonoBehaviour
 
     public int currentFloor = 0;
     public float speed = 3f;
-    public TMP_Text floorDisplay; // Drag your text here later
+    public float yOffset = 0.75f; // The offset to make it sit ON the floor
+    public TMP_Text floorDisplay;
 
     private float groundFloorY = -4f;
     private float floorGap = 2f;
@@ -19,8 +20,8 @@ public class Elevator : MonoBehaviour
 
     void Start()
     {
-        // Set starting position based on current floor
-        transform.position = new Vector2(transform.position.x, groundFloorY + (currentFloor * floorGap));
+        // Added yOffset here
+        transform.position = new Vector2(transform.position.x, groundFloorY + (currentFloor * floorGap) + yOffset);
         UpdateDisplay();
     }
 
@@ -41,7 +42,8 @@ public class Elevator : MonoBehaviour
         if (requests.Count > 0)
         {
             int nextFloor = requests.Dequeue();
-            targetPos = new Vector2(transform.position.x, groundFloorY + (nextFloor * floorGap));
+            // Added yOffset here
+            targetPos = new Vector2(transform.position.x, groundFloorY + (nextFloor * floorGap) + yOffset);
             currentState = ElevatorState.Moving;
             StartCoroutine(MoveToFloor(nextFloor));
         }
@@ -56,7 +58,8 @@ public class Elevator : MonoBehaviour
         while (Mathf.Abs(transform.position.y - targetPos.y) > 0.01f)
         {
             transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
-            currentFloor = Mathf.RoundToInt((transform.position.y - groundFloorY) / floorGap);
+            // Account for yOffset when calculating the display number
+            currentFloor = Mathf.RoundToInt((transform.position.y - groundFloorY - yOffset) / floorGap);
             UpdateDisplay();
             yield return null;
         }
@@ -65,7 +68,6 @@ public class Elevator : MonoBehaviour
         currentFloor = targetFloor;
         UpdateDisplay();
 
-        // Pause briefly to simulate doors opening/loading
         yield return new WaitForSeconds(1f);
         ProcessNextRequest();
     }
